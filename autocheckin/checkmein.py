08c552891e52348c81dc913de6214a6ff3aef458
bahmanh <bahmanhayat@gmail.com>
2015-12-24 20:58:05 -0800

    add checkIn() function

--- autocheckin/checkmein.py
+++ autocheckin/checkmein.py
@@ -3,9 +3,23 @@ from selinium.webdriver.common.keys import Keys
 
 class CheckMeIn(object): 
     
-
     def __init__(self, firstName, lastName, confNum):
         self.firstName = firstName
         self.lastName = lastName
         self.confNum = confNum
 
+    def checkIn():
+        driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs', service_log_path='/dev/null')
+        driver.get("https://www.southwest.com/flight/retrieveCheckinDoc.html")
+        
+        firstNameElem = driver.find_element_by_id("firstName")
+        lastNameElem = driver.find_element_by_id("lastName")
+        confNumElem = driver.find_element_by_id("confirmationNumber")
+        buttonElem = driver.find_element_by_id("submitButton")
+        
+        firstNameElem.send_keys(self.firstName)
+        lastNameElem.send_keys(self.lastName)
+        confNumElem.send_keys(self.confNum)
+        buttonElem.click()
+        
+        driver.close()
