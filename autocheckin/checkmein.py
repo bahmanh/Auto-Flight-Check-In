834e18e18f8a64e2ad4499125ccf6465c0a3819a
bahmanh <bahmanhayat@gmail.com>
2016-01-17 18:30:04 -0800

    add functionality to catch incorrect details errors

--- autocheckin/checkmein.py
+++ autocheckin/checkmein.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import sys
 
 class CheckMeIn(object): 
     
@@ -11,12 +16,13 @@ class CheckMeIn(object):
     #Using PhantomJS instead of Firefox so that this script can be run without a gui in the background
     def checkIn(self):
         try:
+            #service_log_path is set so no logs are generated
             driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs', service_log_path='/dev/null')
             driver.get("https://www.southwest.com/flight/retrieveCheckinDoc.html")
         except selenium.common.exceptions.WebDriverException as error:
             print error.msg
             print "PhantomJS not found"
-            exit(0)
+            #sys.exit(1)
 
         print "\nSuccessfully logged on \n"
 
@@ -24,17 +30,25 @@ class CheckMeIn(object):
         firstNameElem = driver.find_element_by_id("firstName")
         lastNameElem = driver.find_element_by_id("lastName")
         confNumElem = driver.find_element_by_id("confirmationNumber")
-        buttonElem = driver.find_element_by_id("submitButton")
-        
+        submitButtonElem = driver.find_element_by_id("submitButton")
+         
         print "Entering Data \n"
 
         #Sending all data to webpage 
         firstNameElem.send_keys(self.firstName)
         lastNameElem.send_keys(self.lastName)
         confNumElem.send_keys(self.confNum)
-        buttonElem.click()
+        submitButtonElem.click()
         
-        print "Done \n"
-        driver.close()
-        driver.quit()
-
+        #next page
+        try: 
+            checkinButtonElem = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.ID, "printDocumentsButton"))
+            )
+            checkinButtonElem.click()
+        except TimeoutException:
+            print "***Incorrect name or confirmation number. Try again!***"
+        finally:
+            print "Done \n"
+            driver.close()
+            driver.quit()
